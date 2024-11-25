81c4a1ee5d76ca2f90a8e2043fb74321d1c60805
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-11-25 09:11:40 +0530

    Update queue.py

--- VenomX/utils/inline/queue.py
+++ VenomX/utils/inline/queue.py
@@ -25,10 +25,10 @@ def queue_markup(
         ],
          [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"tg://openmessage?user_id={OWNER_ID}",
+                text="❤️‍🔥", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=SUPPORT_CHAT,
+                text="🪫", url=SUPPORT_CHAT,
             ),
         ],
     ]
@@ -51,10 +51,10 @@ def queue_markup(
         ],
          [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"tg://openmessage?user_id={OWNER_ID}",
+                text="❤️‍🔥", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=SUPPORT_CHAT,
+                text="🪫", url=SUPPORT_CHAT,
             ),
         ],
     ]
@@ -77,10 +77,10 @@ def queue_back_markup(_, CPLAY):
            ],
              [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"tg://openmessage?user_id={OWNER_ID}",
+                text="❤️‍🔥", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=SUPPORT_CHAT,
+                text="🪫", url=SUPPORT_CHAT,
             ),
         ],
         ]
@@ -91,17 +91,17 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="❤️‍🔥", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="👻", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="🪫", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="🎸", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
          [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"tg://openmessage?user_id={OWNER_ID}",
+                text="", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=SUPPORT_CHAT,
+                text="", url=SUPPORT_CHAT,
             ),
         ],
     ]
